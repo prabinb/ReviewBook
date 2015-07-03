5fe4b57e9d2c344784e45c19e2d2ea7041d5662f
shankark <shankar.karingula@imaginea.com>
2015-07-03 21:33:22 +0530

    fix

--- app/controllers/Reviews.java
+++ app/controllers/Reviews.java
@@ -1,9 +1,8 @@
 package controllers;
 
-import api.results.APIResult;
-import api.results.Result;
 import play.libs.Json;
 import play.mvc.Controller;
+import play.mvc.Result;
 import services.UserService;
 import services.impl.UserServiceImpl;
 
@@ -12,7 +11,7 @@ public class Reviews extends Controller {
 public Result getAllReviews()
 {
  UserService userService = new UserServiceImpl();	
-return (Result) ok(Json.toJson(userService.getAllReviews()));
+return ok(Json.toJson(userService.getAllReviews()));
 }
 
 }
