ea209ae903ebab68657ec16fd379bf4248d5ae55
Prabin <prabin.banka@imaginea.com>
2015-07-03 08:27:03 +0530

    Created skeleton of the project

---
+++ app/controllers/Application.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Application extends Controller {
+
+  public Result index() {
+    return ok(views.html.index.render("Your new application is ready."));
+  }
+}
