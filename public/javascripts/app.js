98f849f12243ac1dc4465f81670611aae2cda9d6
bharatraja <bharatraj.arutla@imaginea.com>
2015-07-05 16:25:39 +0530

    navigation bar highlighting

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,7 @@
 $(function(){
 	loadTrendingReviews();
+	bindEvents();
+	
 });
 
 
@@ -8,6 +10,29 @@ function loadTrendingReviews() {
 	reviewModelList.fetch({
 		success : function(model, response, options){
 			new ReviewView().renderReviews(model.models);
+		}
+	});
+}
+
+function bindEvents(){
+	bindNavigationEvents();
+}
+
+function bindNavigationEvents(){
+	$('.lef_nav a').on("click", function(e) {
+		var $this = $(this);
+		$this.addClass('active');
+		switch($this.attr('action')){
+			case 'postReview' :
+				$this.parents('.lef_nav').find('a[action!=postReview]').removeClass('active');
+				showPostReviewSection();
+				break;
+			case 'myReviews' : 
+				$this.parents('.lef_nav').find('a[action!=myReviews]').removeClass('active');
+				break;
+			case 'myProfile' :
+				$this.parents('.lef_nav').find('a[action!=myProfile]').removeClass('active');
+				break;
 		}
 	});
 }
