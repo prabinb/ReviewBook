5e240864688082af148ed91416d5fafbaf7b2be2
bharatraja <bharatraj.arutla@imaginea.com>
2015-07-05 15:07:40 +0530

    UI changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,13 @@
 $(function(){
+	loadTrendingReviews();
+});
+
+
+function loadTrendingReviews() {
 	var reviewModelList = new ReviewModelList();
 	reviewModelList.fetch({
 		success : function(model, response, options){
 			new ReviewView().renderReviews(model.models);
 		}
 	});
-});
+}
